d45b90d37c50fc360d0200bb320444044b715880
Aro <otso.aro@cgi.com>
2018-07-24 14:57:22 +0300

    configuration changes

--- models/person.js
+++ models/person.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose')
 
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
 const url = process.env.MONOGODB_URI
 
 mongoose.connect(url)
